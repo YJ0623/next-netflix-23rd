d9239a515cf653ea03770d5952920bcefd86955e
YJ <hghk0046@gmail.com>
2026-04-28 15:18:27 +0900

    Pakage: 절대경로 alias 설정

--- ceos13th_netflix/app/page.tsx
+++ ceos13th_netflix/app/page.tsx
@@ -1,14 +1,6 @@
 export default function Home() {
   return (
     <div className="">
-      <p className="text-heading1">헤딩1</p>
-      <p className="text-heading2">헤딩2</p>
-      <p className="text-body1">바디1</p>
-      <p className="text-body2">바디2</p>
-      <p className="text-label1">라벨1</p>
-      <p className="text-label2">라벨2</p>
-      <p className="text-caption1">캡션1</p>
-      <p className="text-caption2">캡션2</p>
       
     </div>
   );
