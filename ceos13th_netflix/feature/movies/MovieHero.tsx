3b01acc6804ddd7d4dfb7af118a2db075c683dc5
a-00-a <sy.lee.2391@gmail.com>
2026-05-02 12:11:02 +0900

    Feat: TOP10 svg 배지 추가 및 UI 반영

--- ceos13th_netflix/feature/movies/MovieHero.tsx
+++ ceos13th_netflix/feature/movies/MovieHero.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { Movie } from '@/feature/movies/services/movieApi';
+import top10 from '@/public/icon_top10.svg';
 
 interface HeroCarouselProps {
   movies: Movie[];
@@ -33,18 +34,24 @@ export default function HeroCarousel({ movies }: HeroCarouselProps) {
               src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
               alt={movie.title}
               fill
-              sizes='100vw'
+              sizes="100vw"
               className="object-cover"
-              priority={index === 0} 
+              priority={index === 0}
             />
             <div className="absolute inset-0 bg-linear-to-t from-black via-black/10 to-transparent" />
-            <div className='absolute bottom-0.5 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-label2 font-bold z-10'>
-              #{index + 1} in Korea today
+            <div className="absolute bottom-0.5 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-label2 font-bold z-10 flex items-center gap-1.5">
+              <Image
+                src={top10}
+                alt="top10"
+                width={15}
+                height={15}
+                className="shrink-0"
+              />
+              <span>#{index + 1} in Korea today</span>
             </div>
           </div>
         ))}
       </div>
-      
     </div>
   );
 }
