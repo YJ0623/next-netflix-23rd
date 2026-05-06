7e682b0e1c8d78815f11c850c3a23c2831470b71
a-00-a <sy.lee.2391@gmail.com>
2026-05-06 17:45:27 +0900

    Refactor: Image 기반 SVG 아이콘을 SVGR 방식으로 전환

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import icon_netflix from '@/public/icon_netflix.svg';
+import NetflixIcon from '@/public/icon_netflix.svg';
 
 export default function Header() {
-    return (
-        <header className="flex items-center justify-center w-full h-15 gap-6">
-            <Image src={icon_netflix} alt="netfilx icon" width={56} priority/>
-            <a className="text-white text-body1">TV Shows</a>
-            <a className="text-white text-body1">Movies</a>
-            <a className="text-white text-body1">My List</a>
-        </header>
-    )
+  return (
+    <header className="flex items-center justify-center w-full h-15 gap-6">
+      <NetflixIcon className="w-14 h-auto" />
+      <a className="text-white text-body1">TV Shows</a>
+      <a className="text-white text-body1">Movies</a>
+      <a className="text-white text-body1">My List</a>
+    </header>
+  );
 }
